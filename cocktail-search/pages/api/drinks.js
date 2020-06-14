e8c514cf44f0289e6072b76f1711c770ea6b4c7a
Ally S <ally.lobova@gmail.com>
2020-06-13 22:22:21 -0400

    data rendering omg yay

--- cocktail-search/pages/api/drinks.js
+++ cocktail-search/pages/api/drinks.js
@@ -1,3 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import firebase from 'firebase';
-import { FirbaseWrapper } from '../../lib/db/firebase';
+import { FirebaseWrapper } from '../../lib/db/firebase';
+
+export default async (req, res) => {
+  FirebaseWrapper.GetInstance().Initialize(config);
+  try {
+    const allDrinks = await FirebaseWrapper.GetInstance().GetAllDrinks();
+    console.log(allDrinks);
+    res.json(allDrinks);
+  } catch (error) {
+    console.log(error);
+  }
+};
