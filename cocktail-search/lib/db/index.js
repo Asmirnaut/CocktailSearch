02cef22854cf435c3dbe18b746764336fcc9b3af
Ally S <ally.lobova@gmail.com>
2020-06-13 20:09:46 -0400

    drinks in firestore

--- cocktail-search/lib/db/index.js
+++ cocktail-search/lib/db/index.js
@@ -1,19 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import { FIREBASE_API_KEY } from '../../secret';
 
-export default function loadFirebase() {
-  // Initialize Firebase
-  const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATA_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  };
-  // Initialize Firebase
-  firebase.initializeApp(config);
-  return firebase;
-}
+// Initialize Firebase
+export const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: 'cocktailsearch.firebaseapp.com',
+  databaseURL: 'https://cocktailsearch.firebaseio.com',
+  projectId: 'cocktailsearch',
+  storageBucket: 'cocktailsearch.appspot.com',
+  messagingSenderId: '811566660421',
+  appId: '1:811566660421:web:f2cffdef75183125b25c2c',
+  measurementId: 'G-CNHMJC07XJ',
+};
